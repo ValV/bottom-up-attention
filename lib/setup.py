155cadc146aa5003504c8b3979459ef1085c05b7
Vladimir Valeyev <ValV@users.noreply.github.com>
2023-05-05 07:40:30 +0300

    Update setup.py

--- lib/setup.py
+++ lib/setup.py
@@ -5,26 +5,27 @@
 # Written by Ross Girshick
 # --------------------------------------------------------
 
-import os
-from os.path import join as pjoin
+from os import environ, path as osp
 from setuptools import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
-import subprocess
+
 import numpy as np
 
-def find_in_path(name, path):
-    "Find a file in a search path"
-    # Adapted fom
+from Cython.Distutils import build_ext
+
+
+def find_in_path(name, paths):
+    """Find a file in a search path"""
+    # Adapted from
     # http://code.activestate.com/recipes/52224-find-a-file-given-a-search-path/
-    for dir in path.split(os.pathsep):
-        binpath = pjoin(dir, name)
-        if os.path.exists(binpath):
-            return os.path.abspath(binpath)
+    for path in paths.split(osp.sep):
+        path_bin = osp.join(path, name)
+        if osp.exists(path_bin):
+            return osp.abspath(path_bin)
     return None
 
 
-def locate_cuda():
+def locate_cuda(cudaconfig):
     """Locate the CUDA environment on the system
 
     Returns a dict with keys 'home', 'nvcc', 'include', and 'lib64'
@@ -35,34 +36,52 @@ def locate_cuda():
     """
 
     # first check if the CUDAHOME env variable is in use
-    if 'CUDAHOME' in os.environ:
-        home = os.environ['CUDAHOME']
-        nvcc = pjoin(home, 'bin', 'nvcc')
-    else:
+    default_path = osp.join(osp.sep, 'usr', 'local', 'cuda', 'bin')
+    if 'CUDAHOME' in environ:
+        home = environ['CUDAHOME']
+        nvcc = osp.join(home, 'bin', 'nvcc')
+    elif osp.isdir(default_path):
         # otherwise, search the PATH for NVCC
-        default_path = pjoin(os.sep, 'usr', 'local', 'cuda', 'bin')
-        nvcc = find_in_path('nvcc', os.environ['PATH'] + os.pathsep + default_path)
+        nvcc = find_in_path('nvcc', environ['PATH'] + osp.sep + default_path)
         if nvcc is None:
             raise EnvironmentError('The nvcc binary could not be '
-                'located in your $PATH. Either add it to your path, or set $CUDAHOME')
-        home = os.path.dirname(os.path.dirname(nvcc))
+                                   'located in your $PATH. Either add it to '
+                                   'your path, or set $CUDAHOME')
+        home = osp.dirname(osp.dirname(nvcc))
+    else:
+        home = ''
+        nvcc = ''
 
-    cudaconfig = {'home':home, 'nvcc':nvcc,
-                  'include': pjoin(home, 'include'),
-                  'lib64': pjoin(home, 'lib64')}
-    for k, v in cudaconfig.items():
-        if not os.path.exists(v):
-            raise EnvironmentError('The CUDA %s path could not be located in %s' % (k, v))
+    if cudaconfig['home']:
+        cudaconfig.update({
+            'home': home,
+            'nvcc': nvcc,
+            'include': osp.join(home, 'include'),
+            'lib64': osp.join(home, 'lib64')
+        })
+        for k, v in cudaconfig.items():
+            if not osp.exists(v):
+                raise EnvironmentError(f"The CUDA {k} path could not be located"
+                                       f" in {v}")
 
     return cudaconfig
-CUDA = locate_cuda()
 
+
+CUDA = {
+    'home': '',
+    'nvcc': '',
+    'include': '',
+    'lib64': ''
+}
+
+locate_cuda(CUDA)
 
 # Obtain the numpy include directory.  This logic works across numpy versions.
 try:
     numpy_include = np.get_include()
 except AttributeError:
     numpy_include = np.get_numpy_include()
+
 
 def customize_compiler_for_nvcc(self):
     """inject deep into distutils to customize how the dispatch
@@ -85,7 +104,7 @@ def customize_compiler_for_nvcc(self):
     # object but distutils doesn't have the ability to change compilers
     # based on source extension: we add it.
     def _compile(obj, src, ext, cc_args, extra_postargs, pp_opts):
-        if os.path.splitext(src)[1] == '.cu':
+        if osp.splitext(src)[1] == '.cu':
             # use the cuda for .cu files
             self.set_executable('compiler_so', CUDA['nvcc'])
             # use only a subset of the extra_postargs, which are 1-1 translated
@@ -114,15 +133,16 @@ ext_modules = [
         "utils.cython_bbox",
         ["utils/bbox.pyx"],
         extra_compile_args={'gcc': ["-Wno-cpp", "-Wno-unused-function"]},
-        include_dirs = [numpy_include]
+        include_dirs=[numpy_include]
     ),
     Extension(
         "nms.cpu_nms",
         ["nms/cpu_nms.pyx"],
         extra_compile_args={'gcc': ["-Wno-cpp", "-Wno-unused-function"]},
-        include_dirs = [numpy_include]
-    ),
-    Extension('nms.gpu_nms',
+        include_dirs=[numpy_include]
+    ) if CUDA['home'] else
+    Extension(
+        'nms.gpu_nms',
         ['nms/nms_kernel.cu', 'nms/gpu_nms.pyx'],
         library_dirs=[CUDA['lib64']],
         libraries=['cudart'],
@@ -131,20 +151,25 @@ ext_modules = [
         # this syntax is specific to this build system
         # we're only going to use certain compiler args with nvcc and not with
         # gcc the implementation of this trick is in customize_compiler() below
-        extra_compile_args={'gcc': ["-Wno-unused-function"],
-                            'nvcc': ['-arch=sm_35',
-                                     '--ptxas-options=-v',
-                                     '-c',
-                                     '--compiler-options',
-                                     "'-fPIC'"]},
-        include_dirs = [numpy_include, CUDA['include']]
+        extra_compile_args={
+            'gcc': ["-Wno-unused-function"],
+            'nvcc': [
+                '-arch=sm_35',
+                '--ptxas-options=-v',
+                '-c',
+                '--compiler-options',
+                "'-fPIC'"
+            ]
+        },
+        include_dirs=[numpy_include, CUDA['include']]
     ),
     Extension(
         'pycocotools._mask',
         sources=['pycocotools/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs = [numpy_include, 'pycocotools'],
+        include_dirs=[numpy_include, 'pycocotools'],
         extra_compile_args={
-            'gcc': ['-Wno-cpp', '-Wno-unused-function', '-std=c99']},
+            'gcc': ['-Wno-cpp', '-Wno-unused-function', '-std=c99']
+        },
     ),
 ]
 
